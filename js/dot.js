0b690cfb967d9409063cb7810f092fbde3e0e326
judas <gonza.drumstick@gmail.com>
2018-07-06 00:10:14 -0300

    initial commit

---
+++ js/dot.js
@@ -0,0 +1,62 @@
+$(document).ready(function() { 
+        //menu desplegable
+
+        
+        
+        //efecto-menu
+        $(".nav a").each(function(index, el) {
+            $(el).css({ opacity: 0, marginRight: -100 });
+            $(el).animate({opacity: 1, marginRight: 0}, 1500 + (index*500) );
+        });
+       
+        //efecto logo
+        var tituloCiudad = $(".titulo, .ciudad")
+        tituloCiudad.css({opacity: 0,});
+        tituloCiudad.animate({opacity: 1}, 4000);
+        
+        $(".texto-logo")
+            .addClass("texto-logo--loaded")
+            .css({opacity: 0})
+            .animate({opacity: 1}, 3000);
+
+
+        //navegar offset
+
+        var inicio = $("#inicio").offset().top,
+            works = $("#works").offset().top,
+            dotTeam = $("#dot-team").offset().top,
+            loQueHacemos = $("#loQueHacemos").offset().top,
+            contacto = $("#contacto").offset().top;
+
+        $("#btn-inicio").on('click', function(e) {
+            e.preventDefault();
+            $("html, body").animate({scrollTop: inicio }, 1000);});
+    
+        $("#btn-works").on('click', function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: works
+            }, 1000);
+        });
+        $("#btn-dot-team").on('click', function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: dotTeam 
+            }, 1000);
+        });
+        $("#btn-loQueHacemos").on('click', function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: loQueHacemos 
+            }, 1000);
+        });
+        $("#btn-contacto").on('click', function(e) {
+            e.preventDefault();
+            $("html, body").animate({
+                scrollTop: contacto + 150
+            }, 1000);
+        });      
+        
+       
+
+});
